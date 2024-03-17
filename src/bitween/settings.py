384c1790c9d5c5427b69f08fa61185b7c581234c
Ferhat Erata <ferhat.erata@yale.edu>
2024-03-17 00:40:49 -0400

    add new settings

--- src/bitween/settings.py
+++ src/bitween/settings.py
@@ -2,18 +2,24 @@ LOGGER_LEVEL = 3
 
 DEGREE = 2
 
-MULTIPLE_REGRESSION = False
+DELTA = 0.2  # Property Test threshold
+
+# NOTE: Multiple Regression Method
+MULTIPLE_REGRESSION = False  # if True, then we use the multiple regression methods
 CROSS_VALIDATION = 5
 
-MILP = False  # use MILP solver
+# NOTE: Regression Refinement Method
+REGRESSION_REFINEMENT = False  # if True, then use the regression refinement algorithm
+
+# NOTE: MILP Method parameters
+MILP = False  # if True, then we use the MILP solver after the regression-based methods
 OBJECTIVE_THRESHOLD = 1e-9
 MILP_BOUND = 12
 
-
-UGLY_FACTOR = 20  # remove equalities that have lots of terms and "large" coefficients
-# coeff_threshold
+# NOTE: Equation inference parameters for Regression-based Methods
 COEFF_THRESHOLD = 0.3  # remove coefficients that are smaller than this value
 COEFF_CUTOFF = 30  # remove equalities that are larger than this value
 INTERCEPT_CUTOFF = 50  # remove equalities that are larger than this value
-# MILP: don't include objective if it's greater than this value
-DELTA = 0.2  # Property Test threshold
+
+# NOTE: Reductions
+UGLY_FACTOR = 20  # remove equalities that have lots of terms and "large" coefficients
