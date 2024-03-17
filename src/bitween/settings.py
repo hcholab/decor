447934d93d9582ddbab5b203a8a001b1a5b213d0
Ferhat Erata <ferhat.erata@yale.edu>
2024-03-16 22:08:34 -0400

    update default settings

--- src/bitween/settings.py
+++ src/bitween/settings.py
@@ -1,6 +1,6 @@
 LOGGER_LEVEL = 3
 
 DEGREE = 2
-MILP = True  # use MILP solver
+MILP = False  # use MILP solver
 
 UGLY_FACTOR = 20  # remove equalities that have lots of terms and "large" coefficients
