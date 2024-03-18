c4db0d9cbdaa110022c1435f2adbdd9405dc9b94
Ferhat Erata <ferhat.erata@yale.edu>
2024-03-18 00:08:32 -0400

    update settings

--- src/bitween/settings.py
+++ src/bitween/settings.py
@@ -1,14 +1,16 @@
-FILE_PATH = "benchmarks/bitween/dig/bresenham.dig.dyn.traces"
-# file_path = "benchmarks/bitween/dig/cohencu.dig.dyn.traces"
-# file_path = "benchmarks/bitween/dig/cohendiv.dig.dyn.traces"
-# file_path = "benchmarks/bitween/dig/dijkstra.dig.dyn.traces"
-# file_path = "benchmarks/bitween/dig/egcd.dig.dyn.traces"
+# FILE_PATH = "benchmarks/bitween/dig/bresenham.dig.dyn.traces"
+FILE_PATH = "benchmarks/bitween/dig/cohencu.dig.dyn.traces"
+# FILE_PATH = "benchmarks/bitween/dig/cohendiv.dig.dyn.traces"
+# FILE_PATH = "benchmarks/bitween/dig/dijkstra.dig.dyn.traces"
+# FILE_PATH = "benchmarks/bitween/dig/egcd.dig.dyn.traces"
 
 LOGGER_LEVEL = 3
 
 DEGREE = 2
 
 DELTA = 0.2  # Property Test threshold
+
+TYPE = "INT"  # INT or REAL
 
 # NOTE: Multiple Regression Method
 MULTIPLE_REGRESSION = False  # if True, then we use the multiple regression methods
@@ -18,16 +20,19 @@ CROSS_VALIDATION = 5
 REGRESSION_REFINEMENT = False  # if True, then use the regression refinement algorithm
 
 # NOTE: MILP Method parameters
-MILP = True  # if True, then we use the MILP solver after the regression-based methods
+MILP = False  # if True, then we use the MILP solver after the regression-based methods
 OBJECTIVE_THRESHOLD = 1e-9
 MILP_BOUND = 12
 MILP_TIME_LIMIT = 3  # in seconds
 PARALLEL_MILP = True  # if True, then we use the parallel MILP solver
 
 # NOTE: Equation inference parameters for Regression-based Methods
-COEFF_THRESHOLD = 0.3  # remove coefficients that are smaller than this value
+COEFF_THRESHOLD = 0.1  # remove coefficients that are smaller than this value
 COEFF_CUTOFF = 30  # remove equalities that are larger than this value
 INTERCEPT_CUTOFF = 50  # remove equalities that are larger than this value
 
 # NOTE: Reductions
 UGLY_FACTOR = 20  # remove equalities that have lots of terms and "large" coefficients
+
+# NOTE: Z3
+SOLVER_TIMEOUT = 10  # in seconds
