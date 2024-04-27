543ed78edf4e019f7487ce9c2bc2f7f9fbaf21ef
Ferhat Erata <ferhat.erata@yale.edu>
2024-04-26 21:52:56 -0400

    add evaluation script for fpbench salsa benchmarks

---
+++ src/bitween/evaluation_fpbench_salsa.py
@@ -0,0 +1,29 @@
+from bitween.main import infer_invariants
+from bitween import settings
+
+import time
+
+
+def odometry():
+    file_path = "./benchmarks/bitween/fpcore/salsa.c"
+    func_name = "Odometry"
+    infer_invariants(file_path, func_name, max_degree=2, n=10, milp=solver)
+
+
+def pid():
+    file_path = "./benchmarks/bitween/fpcore/salsa.c"
+    func_name = "PID"
+    infer_invariants(file_path, func_name, max_degree=3, n=5, milp=solver)
+
+
+if __name__ == "__main__":
+
+    solver = settings.MILPSolver.GUROBI
+
+    start = time.time()
+
+    # odometry()
+    pid()
+
+    end = time.time()
+    print(f"Total time: {end - start}")
