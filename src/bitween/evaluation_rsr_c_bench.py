e19c85057b32a395ebb90a38ae139e559093a57b
Ferhat Erata <ferhat.erata@yale.edu>
2024-05-05 19:43:28 -0400

    introduce a version of rsr bench using C code

--- src/bitween/evaluation_rsr_c_bench.py
+++ src/bitween/evaluation_rsr_c_bench.py
@@ -58,6 +58,15 @@ def sin():
             assert verify(eqt.lhs, f)
 
 
+def sin_taylor():
+    file_path = "./benchmarks/bitween/rsr-benchs/sine_taylor.c"
+    func_name = "sine_taylor"
+
+    infer_invariants_and_verify_correctness(
+        file_path, func_name, max_degree=2, n=150, milp=glpk
+    )
+
+
 if __name__ == "__main__":
 
     glpk = settings.MILPSolver.GLPK
@@ -66,6 +75,7 @@ if __name__ == "__main__":
 
     # identity()
     # exp()
-    sin()
+    # sin() # NOTE: civl cannot verify this, therefore we use our own verify function
+    sin_taylor()  # NOTE: civl verifies this
 
     log.debug(f"Total Time: {time() - st:.2f}s")
