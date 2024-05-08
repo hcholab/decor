068a3226be922b06888a45a8a66c3d65fe735b3d
Ferhat Erata <ferhat.erata@yale.edu>
2024-05-07 23:50:26 -0400

    introduce float bench evaluation

--- src/bitween/evaluation_float_bench.py
+++ src/bitween/evaluation_float_bench.py
@@ -1,91 +1,28 @@
-import math
-import sympy
+from bitween.main import (  # noqa F401
+    infer_invariants_and_check_correctness,
+    infer_invariants_and_verify_correctness,
+)
+from bitween import miscs, settings
 
-from bitween.main import infer_property, verify
-from bitween.sampler import Domain, Distribution
+from time import time
+
+log = miscs.getLogger(__name__, settings.LOGGER_LEVEL)
 
 
-def test_sin():
-    def F(x, terms=40):
-        """sinTaylor"""
-        result = 0.0
-
-        for n in range(terms):
-            numerator = (-1) ** n
-            denominator = 1
-
-            for i in range(1, 2 * n + 2):
-                denominator *= i
-
-            term = numerator * (x ** (2 * n + 1)) / denominator
-            result += term
-
-        return result
-
-    equations = infer_property(
-        Domain.Real,
-        Distribution.Small,
-        ["F(x+y)", "F(x-y)", "F(x)", "F(y)"],  # how to call functions
-        ["f(x+y)", "f(x-y)", "f(x)", "f(y)"],  # function basis aka the template
-        F,
-        max_degree=2,
-        n=150,
+def addsub():
+    file_path = "./benchmarks/bitween/float-benchs/_addsub.c"
+    func_name = "addsub"
+    infer_invariants_and_check_correctness(
+        file_path, func_name, max_degree=2, n=20, milp=glpk
     )
 
-    def f(x):
-        return sympy.sin(x)
 
-    for eq in equations:
-        assert verify(eq, f)
+if __name__ == "__main__":
 
+    glpk = settings.MILPSolver.GLPK
 
-def test_sqrt():
+    st = time()
 
-    def F(x):
-        return math.sqrt(x)
+    addsub()
 
-    equations = infer_property(
-        Domain.Real,
-        Distribution.Small,
-        # [MILP(k=4, timeout=3, max_bound=10, obj=1e-12)],  # methods
-        ["F(x)", "F(x+1)", "x", "1"],
-        ["f(x)", "f(x+1)", "x", "1"],
-        F,
-        max_degree=2,
-        n=30,
-    )
-    # NOTE: x - f(x + 1)**2 + 1 = 0
-    # NOTE: f(x)**2 - f(x + 1)**2 + 1 = 0
-    # NOTE: x - f(x)**2 = 0
-
-    def f(x):
-        return sympy.sqrt(x)
-
-    for eq in equations:
-        assert verify(eq, f)
-
-
-def test_sin_periodic():
-    def f(x):
-        return math.sin(x)
-
-    equations = infer_property(
-        Domain.Real,
-        Distribution.Small,
-        ["f(x+pi)", "f(x-pi)", "f(x)", "f(-x)", "1"],
-        ["f(x+pi)", "f(x-pi)", "f(x)", "f(-x)", "1"],
-        f,
-        max_degree=1,
-        n=30,
-        epsilon=1e-13,
-    )
-
-    def f(x):
-        return sympy.sin(x)
-
-    for eq in equations:
-        assert verify(eq, f)
-
-
-test_sin()
-test_sqrt()
+    log.debug(f"Total Time: {time() - st:.2f}s")
