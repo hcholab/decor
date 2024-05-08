937c9aaaa936e900275033850190a246c8bb1490
Ferhat Erata <ferhat.erata@yale.edu>
2024-05-08 00:10:41 -0400

    add a new float bench evaluation

--- src/bitween/evaluation_float_bench.py
+++ src/bitween/evaluation_float_bench.py
@@ -12,25 +12,23 @@ log = miscs.getLogger(__name__, settings.LOGGER_LEVEL)
 def addsub():
     file_path = "./benchmarks/bitween/float-benchs/_addsub.c"
     func_name = "_addsub"
-    infer_invariants_and_check_correctness(
+    infer_invariants_and_verify_correctness(
         file_path, func_name, max_degree=2, n=20, milp=glpk
     )
 
 
-# _arctan_Pade.c
 def arctan_Pade():
     file_path = "./benchmarks/bitween/float-benchs/_arctan_Pade.c"
     func_name = "_arctan_Pade"
-    infer_invariants_and_check_correctness(
+    infer_invariants_and_verify_correctness(
         file_path, func_name, max_degree=2, n=20, milp=glpk
     )
 
 
-# _cos_polynomial.c
 def cos_polynomial():
     file_path = "./benchmarks/bitween/float-benchs/_cos_polynomial.c"
     func_name = "_cos_polynomial"
-    infer_invariants_and_check_correctness(
+    infer_invariants_and_verify_correctness(
         file_path, func_name, max_degree=2, n=20, milp=glpk
     )
 
@@ -42,7 +40,7 @@ if __name__ == "__main__":
     st = time()
 
     # addsub()
-    arctan_Pade()
-    # cos_polynomial()
+    # arctan_Pade()
+    cos_polynomial()
 
     log.debug(f"Total Time: {time() - st:.2f}s")
