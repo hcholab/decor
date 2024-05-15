8c8e50346570730617c826292cd993896d75db34
Ferhat Erata <ferhat.erata@yale.edu>
2024-05-15 17:59:19 -0400

    Add functions for inverse subtraction and tangent addition using Taylor series

--- src/bitween/evaluation_rsr_c_bench.py
+++ src/bitween/evaluation_rsr_c_bench.py
@@ -158,6 +158,33 @@ def inv_cot_add_taylor():
     )
 
 
+def inv_sub_taylor():
+    file_path = "./benchmarks/bitween/rsr-benchs/inv_sub_taylor.c"
+    func_name = "inv_sub_taylor"
+
+    infer_invariants_and_verify_correctness(
+        file_path, func_name, max_degree=3, n=150, milp=glpk
+    )
+
+
+def inv_sub2_taylor():
+    file_path = "./benchmarks/bitween/rsr-benchs/inv_sub2_taylor.c"
+    func_name = "inv_sub2_taylor"
+
+    infer_invariants_and_verify_correctness(
+        file_path, func_name, max_degree=3, n=150, milp=glpk
+    )
+
+
+def inv_tan_add_taylor():
+    file_path = "./benchmarks/bitween/rsr-benchs/inv_tan_add_taylor.c"
+    func_name = "inv_tan_add_taylor"
+
+    infer_invariants_and_verify_correctness(
+        file_path, func_name, max_degree=3, n=150, milp=glpk
+    )
+
+
 if __name__ == "__main__":
 
     glpk = settings.MILPSolver.GLPK
@@ -179,6 +206,9 @@ if __name__ == "__main__":
     # sinh_taylor()
     # tanh_taylor()
     # cot_taylor()
-    inv_cot_add_taylor()
+    # inv_cot_add_taylor()
+    # inv_sub_taylor()
+    # inv_sub2_taylor()
+    inv_tan_add_taylor()
 
     log.debug(f"Total Time: {time() - st:.2f}s")
