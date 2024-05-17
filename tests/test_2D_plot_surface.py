969c0fdf5ec15cb5a4be41e43c37c639bfdfe837
Ferhat Erata <ferhat.erata@yale.edu>
2024-05-17 12:30:28 -0400

    update interpolation playground

--- tests/test_2D_plot_surface.py
+++ tests/test_2D_plot_surface.py
@@ -12,37 +12,44 @@ def f2(x, y):
     return x + 2 * y
 
 
-# Generate x and y values
-x = np.linspace(-5, 5, 100)
-y = np.linspace(-5, 5, 100)
-x, y = np.meshgrid(x, y)
+if __name__ == "__main__":
+    # Generate x and y values
+    x = np.linspace(-5, 5, 100)
+    y = np.linspace(-5, 5, 100)
+    x, y = np.meshgrid(x, y)
 
-# Compute z values for both functions
-z1 = f1(x, y)
-z2 = f2(x, y)
+    # Compute z values for both functions
+    z1 = f1(x, y)
+    z2 = f2(x, y)
 
-# Create the plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
+    # Create the plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
 
-# Plot the first function
-surface1 = ax.plot_surface(x, y, z1, cmap="viridis", alpha=0.6, label="x + xy + y^2")
-# surface1._edgecolors2d = surface1._edgecolor3d
-# surface1._facecolors2d = surface1._facecolor3d
+    # Plot the first function
+    surface1 = ax.plot_surface(
+        x, y, z1, cmap="viridis", alpha=0.6, label="x + xy + y^2"
+    )
+    # surface1._edgecolors2d = surface1._edgecolor3d
+    # surface1._facecolors2d = surface1._facecolor3d
 
-# Plot the second function
-surface2 = ax.plot_surface(x, y, z2, cmap="plasma", alpha=0.6, label="x + 2xy + y^3")
-# surface2._edgecolors2d = surface2._edgecolor3d
-# surface2._facecolors2d = surface2._facecolor3d
+    # Plot the second function
+    surface2 = ax.plot_surface(
+        x, y, z2, cmap="plasma", alpha=0.6, label="x + 2xy + y^3"
+    )
+    # surface2._edgecolors2d = surface2._edgecolor3d
+    # surface2._facecolors2d = surface2._facecolor3d
 
-# Add titles and labels
-ax.set_title("Surface plots of f1(x, y) = x + xy + y^2 and f2(x, y) = x + 2xy + y^3")
-ax.set_xlabel("x")
-ax.set_ylabel("y")
-ax.set_zlabel("f(x, y)")
+    # Add titles and labels
+    ax.set_title(
+        "Surface plots of f1(x, y) = x + xy + y^2 and f2(x, y) = x + 2xy + y^3"
+    )
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("f(x, y)")
 
-# Add legend
-ax.legend()
+    # Add legend
+    ax.legend()
 
-# Show the plot
-plt.show()
+    # Show the plot
+    plt.show()
