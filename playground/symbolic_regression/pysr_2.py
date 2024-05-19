a10902ea45ae95f77a56a601d376c7cc41052b97
Ferhat Erata <ferhat.erata@yale.edu>
2024-05-19 00:41:56 -0400

    update symbolic regression examples

--- playground/symbolic_regression/pysr_2.py
+++ playground/symbolic_regression/pysr_2.py
@@ -3,6 +3,7 @@ import numpy as np  # noqa F401
 from pysr import PySRRegressor
 from sklearn.dummy import check_random_state
 from sympy import simplify
+from multiprocessing import cpu_count
 
 
 rng = check_random_state(0)
@@ -11,22 +12,76 @@ X_train = rng.uniform(-1, 1, 100).reshape(50, 2)
 y_train = X_train[:, 0] ** 2 - X_train[:, 1] ** 2 + X_train[:, 1] - 1
 
 model = PySRRegressor(
-    niterations=40,  # < Increase me for better results
+    niterations=100,  # < Increase me for better results
     binary_operators=["+", "*", "-"],
     # unary_operators=[
     #     "cos",
     #     "exp",
     #     "sin",
+    #     "sqrt",
     #     "inv(x) = 1/x",
     #     # ^ Custom operator (julia syntax)
     # ],
-    extra_sympy_mappings={"inv": lambda x: 1 / x},
+    populations=max(15, cpu_count() * 2),
+    timeout_in_seconds=2 * 60,
+    # extra_sympy_mappings={"inv": lambda x: 1 / x},
     # ^ Define operator for SymPy as well
-    elementwise_loss="loss(prediction, target) = (prediction - target)^2",
+    # elementwise_loss="loss(prediction, target) = (prediction - target)^2",
     # ^ Custom loss function (julia syntax)
-    # select_k_features=10,
+    select_k_features=10,
     # ^ Train on only the 4 most important features
 )
+
+# est = PySRRegressor(
+#     niterations=1_000_000_000,
+#     ncyclesperiteration=2_500,
+#     population_size=100,
+#     populations=max(15, cpu_count() * 2),
+#     # budget 10 minutes for compile time,
+#     # ensuring we can finish within 2 hours:
+#     timeout_in_seconds=2 * 60 * 60 - 10 * 60,
+#     maxsize=40,
+#     maxdepth=20,
+#     binary_operators=["+", "-", "*", "/"],
+#     unary_operators=["sin", "exp", "log", "sqrt"],
+#     constraints={
+#         **dict(
+#             sin=9,
+#             exp=9,
+#             log=9,
+#             sqrt=9,
+#         ),
+#         **{"/": (-1, 9)},
+#     },
+#     nested_constraints=dict(
+#         sin=dict(
+#             sin=0,
+#             exp=1,
+#             log=1,
+#             sqrt=1,
+#         ),
+#         exp=dict(
+#             exp=0,
+#             log=0,
+#         ),
+#         log=dict(
+#             exp=0,
+#             log=0,
+#         ),
+#         sqrt=dict(
+#             sqrt=0,
+#         ),
+#     ),
+#     # prefer multiprocessing:
+#     procs=cpu_count(),
+#     multithreading=False,
+#     batching=True,
+#     batch_size=50,
+#     turbo=True,
+#     weight_optimize=0.001,
+#     adaptive_parsimony_scaling=1_000.0,
+#     parsimony=0.0,
+# )
 
 model.fit(X_train, y_train)
 
