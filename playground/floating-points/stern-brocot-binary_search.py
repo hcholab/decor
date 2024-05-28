acc8e13236b37706b16cbe27dc47d8a9cb1c3d17
Ferhat Erata <ferhat.erata@yale.edu>
2024-05-27 22:32:30 -0400

    use Fraction.limit_fractions

--- playground/floating-points/stern-brocot-binary_search.py
+++ playground/floating-points/stern-brocot-binary_search.py
@@ -10,7 +10,7 @@ class RationalApprox:
         return Fraction(r1.numerator + r2.numerator, r1.denominator + r2.denominator)
 
     @staticmethod
-    def approximate(x, epsilon=1e-15, max_iterations=10, verbose=False):
+    def approximate(x, epsilon=1e-15, max_iterations=15, verbose=False):
         left = Fraction(0)
         right = Fraction(1)
         best = left
@@ -43,42 +43,61 @@ class RationalApprox:
 
         if iterations > max_iterations and verbose:
             print(f"Warning: max iterations reached ({max_iterations})")
-            return Fraction(x).limit_denominator(1000)
+            return Fraction(x).limit_denominator(int(1 / epsilon / 10))
         return best
 
 
 if __name__ == "__main__":
-    print("SB-Tree:", RationalApprox.approximate(0.333334, epsilon=1e-3, verbose=True))
-    print("Fraction:", Fraction(0.333334).limit_denominator(100))
+    x = 0.333334
+    print("SB-Tree:", RationalApprox.approximate(x, epsilon=1e-3, verbose=True))
+    print("Fraction:", Fraction(x).limit_denominator(100))
     print()
 
-    print("SB-Tree:", RationalApprox.approximate(0.2, epsilon=1e-3, verbose=True))
-    print("Fraction:", Fraction(0.2).limit_denominator(100))
+    x = 0.2
+    print("SB-Tree:", RationalApprox.approximate(x, epsilon=1e-3, verbose=True))
+    print("Fraction:", Fraction(x).limit_denominator(100))
     print()
 
-    print("SB-Tree:", RationalApprox.approximate(0.25, epsilon=1e-3, verbose=True))
-    print("Fraction:", Fraction(0.25).limit_denominator(100))
+    x = 0.25
+    print("SB-Tree:", RationalApprox.approximate(x, epsilon=1e-3, verbose=True))
+    print("Fraction:", Fraction(x).limit_denominator(100))
     print()
 
-    print("SB-Tree:", RationalApprox.approximate(0.666667, epsilon=1e-3, verbose=True))
-    print("Fraction:", Fraction(0.666667).limit_denominator(100))
+    x = 0.666667
+    print("SB-Tree:", RationalApprox.approximate(x, epsilon=1e-3, verbose=True))
+    print("Fraction:", Fraction(x).limit_denominator(100))
     print()
 
-    print("SB-Tree:", RationalApprox.approximate(0.1667, epsilon=1e-3, verbose=True))
-    print("Fraction:", Fraction(0.1667).limit_denominator(100))
+    x = 0.1667
+    print("SB-Tree:", RationalApprox.approximate(x, epsilon=1e-3, verbose=True))
+    print("Fraction:", Fraction(x).limit_denominator(100))
     print()
 
-    print("SB-Tree:", RationalApprox.approximate(0.167, epsilon=1e-3, verbose=True))
-    print("Fraction:", Fraction(0.167).limit_denominator(100))
+    x = 0.167
+    print("SB-Tree:", RationalApprox.approximate(x, epsilon=1e-3, verbose=True))
+    print("Fraction:", Fraction(x).limit_denominator(100))
     print()
 
-    print("SB-Tree:", RationalApprox.approximate(0.083, epsilon=1e-3, verbose=True))
-    print("Fraction:", Fraction(0.083).limit_denominator(100))
+    x = 0.083
+    print("SB-Tree:", RationalApprox.approximate(x, epsilon=1e-3, verbose=True))
+    print("Fraction:", Fraction(x).limit_denominator(100))
     print()
 
-    print("SB-Tree:", RationalApprox.approximate(0.999, epsilon=1e-3, verbose=True))
-    print("Fraction:", Fraction(0.999).limit_denominator(100))
+    x = 0.1111
+    print("SB-Tree:", RationalApprox.approximate(x, epsilon=1e-3, verbose=True))
+    print("Fraction:", Fraction(x).limit_denominator(100))
     print()
 
+    x = 1.1111
+    print("SB-Tree:", RationalApprox.approximate(x, epsilon=1e-3, verbose=True))
+    print("Fraction:", Fraction(x).limit_denominator(100))
+    print()
+
+    x = 0.999
+    print("SB-Tree:", RationalApprox.approximate(x, epsilon=1e-3, verbose=True))
+    print("Fraction:", Fraction(x).limit_denominator(100))
+    print()
+
+    x = 1.2
     print("SB-Tree:", RationalApprox.approximate(1.2, epsilon=1e-3, verbose=True))
     print("Fraction:", Fraction(1.2).limit_denominator(100))
